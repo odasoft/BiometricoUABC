46f308df04b219cdd77c351dd63702a39edb6ce7
Jose Adriel <joseadriel93@gmail.com>
2015-06-29 14:04:22 -0700

    Reportes listos

---
+++ Odavision_Admin/Odavision_Admin/Odavision_AdminScreens/AddReport.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Odavision_AdminScreens
+{
+	/// <summary>
+	/// Interaction logic for AddReport.xaml
+	/// </summary>
+	public partial class AddReport : UserControl
+	{
+		public AddReport()
+		{
+			this.InitializeComponent();
+		}
+
+		private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+			// TODO: Add event handler implementation here.
+		}
+	}
+}
