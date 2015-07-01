d59f762f444804e5babdbe2067a76aa0028d0480
Jose Adriel <joseadriel93@gmail.com>
2015-07-01 12:26:58 -0700

    Agregue un chart la pantalla para seleccionar que tipo de reporte se necesitan

---
+++ Odavision_Admin/Odavision_Admin/Odavision_AdminScreens/SelectReportTYpe.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Odavision_AdminScreens
+{
+	/// <summary>
+	/// Interaction logic for SelectReportTYpe.xaml
+	/// </summary>
+	public partial class SelectReportTYpe : UserControl
+	{
+		public SelectReportTYpe()
+		{
+			this.InitializeComponent();
+		}
+
+		private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+		{
+			// TODO: Add event handler implementation here.
+		}
+	}
+}
